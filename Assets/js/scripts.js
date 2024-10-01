928ddad2b63d29b4ceb4303a787c4422aecb65ed
Felipe Rodrigues <contato@feliperodrigues.net>
2024-09-30 21:14:09 -0300

    upload do numero do whatsapp

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,7 +2,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento para automatizar as redes sociais da minha empresa.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
@@ -11,7 +11,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento para a consultoria em Segurança da Informação.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
@@ -20,7 +20,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento para a Mentoria em Informatica.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
@@ -29,7 +29,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento sobre o Desenvolvimento de um site para o meu negocio.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
@@ -38,7 +38,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento sobre o Desenvolvimento de um sistema para o meu negocio.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
@@ -47,7 +47,7 @@
             var nome = document.getElementById("nome").value;
             var sobrenome = document.getElementById("sobrenome").value;
             var email = document.getElementById("email").value;
-            var telefone = "32999775281";
+            var telefone = "32998409798";
             var mensagem = `Olá, meu nome é ${nome} ${sobrenome}, e meu email é: ${email}. Gostaria de fazer o orçamento sobre o Desenvolvimento de uma planilha para o meu negocio.`;
             var link = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
             window.open(link, '_blank');
